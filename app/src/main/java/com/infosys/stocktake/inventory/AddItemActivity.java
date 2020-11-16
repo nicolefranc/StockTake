c2d7a38eb41684bf8f39dda2d8781cb29a8982de
Nicole Yu <nicoleyu@outlook.sg>
2020-11-17 04:55:41 +0800

    Implement upload to Firebase Storage

--- app/src/main/java/com/infosys/stocktake/inventory/AddItemActivity.java
+++ app/src/main/java/com/infosys/stocktake/inventory/AddItemActivity.java
@@ -1,8 +1,10 @@
 package com.infosys.stocktake.inventory;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -11,29 +13,54 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.infosys.stocktake.R;
 
 import java.io.IOException;
 
 public class AddItemActivity extends AppCompatActivity {
     private Button selectImgBtn;
+    private ImageView imagePreview;
+    private Button uploadBtn;
     private final int PICK_IMAGE_REQUEST = 22;
     private Uri filePath;
-    private ImageView imagePreview;
+
+    // Instance for Firebase Storage, Storage Reference
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        imagePreview = findViewById(R.id.imgPreview);
+        // Initialise Firebase Storage, Storage Reference
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
+        // Initialise UI Components
         selectImgBtn = findViewById(R.id.selectImgBtn);
+        imagePreview = findViewById(R.id.imgPreview);
+        uploadBtn = findViewById(R.id.uploadBtn);
+
+        // Event Listeners
         selectImgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AddItemActivity.this.selectImage();
+            }
+        });
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadFile();
             }
         });
     }
@@ -55,6 +82,7 @@ public class AddItemActivity extends AppCompatActivity {
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             filePath = data.getData();
             try {
+                // Convert the image to bitmap to show in ImageView component
                 Bitmap bitmap = MediaStore
                                     .Images
                                     .Media
@@ -63,6 +91,35 @@ public class AddItemActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void uploadFile() {
+        if (filePath != null) {
+            // Upload to Firebase Storage
+            StorageReference ref = storageReference
+                    .child("testingImages/" + System.currentTimeMillis());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(AddItemActivity.this, "Successfully uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(AddItemActivity.this, "Upload FAILED", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            Toast.makeText(AddItemActivity.this, "Uploading...", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show();
         }
     }
 }
