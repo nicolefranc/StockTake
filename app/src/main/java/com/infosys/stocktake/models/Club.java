cd99f6db47c1805749b7c678da5db40afc193c93
Filbert Cia <cia.filbert@gmail.com>
2020-11-23 19:37:11 +0800

    fixed club model

--- app/src/main/java/com/infosys/stocktake/models/Club.java
+++ app/src/main/java/com/infosys/stocktake/models/Club.java
@@ -9,44 +9,23 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+
 public class Club {
     private final String TAG = "CLUB Operations";
     private String clubID;
     private String clubName;
     private int loanCounter;
     //list of club admins' student IDs who have approval
-    private String[] adminList;
+    ArrayList<String> adminList;
 
-    FirebaseFirestore db;
-    CollectionReference clubs;
-    Club queryClub;
-
-    public Club(){
-        loanCounter = getLoanCounter();
-    }
+    public Club(){}
     public Club(String clubID, String clubName){
         this.clubID = clubID;
         this.clubName = clubName;
         loanCounter = 0;
     }
 
-
-    /*getClub to be migrated to activity file
-     */
-    public Club getClub(final String clubID){
-        FirebaseAuth fbAuth = FirebaseAuth.getInstance();
-        db= FirebaseFirestore.getInstance();
-        clubs = db.collection("clubs");
-        DocumentReference clubRef = db.collection("clubs").document(clubID);
-        clubRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                queryClub = documentSnapshot.toObject(Club.class);
-                Log.d(TAG, "CLUB "+ clubID +"has been successfully fetched!");
-            }
-        });
-        return queryClub;
-    }
     public int getLoanCounter(){ return loanCounter;}
     public void increaseLoanCounter(){loanCounter+=1;}
     public String getClubID() {
@@ -65,11 +44,10 @@ public class Club {
         this.clubName = clubName;
     }
 
-    public String[] getAdminList() {
+    public ArrayList<String> getAdminList() {
         return adminList;
     }
 
-    public void setAdminList(String[] adminList) {
-        this.adminList = adminList;
-    }
+    public void addAdmin (String uuid){adminList.add(uuid);}
+    public void removeAdmin (String uuid) {adminList.remove(uuid);}
 }
