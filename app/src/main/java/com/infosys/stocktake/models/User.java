49d8150daf8273368a6d6e46c85b39294798ce20
Cayden <gweeyongta@gmail.com>
2020-12-01 21:17:53 +0800

    Removed getUser from feature/auth branch in User.java

--- app/src/main/java/com/infosys/stocktake/models/User.java
+++ app/src/main/java/com/infosys/stocktake/models/User.java
@@ -49,9 +49,7 @@ public class User implements Serializable {
         }
     }
 
-    public User getUser() {
-        return this;
-    }
+
 
     /***********************************************************************************************
      ACCESSORs
