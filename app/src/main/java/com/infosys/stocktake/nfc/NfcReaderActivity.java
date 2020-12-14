c532c4eaba36ee874b11121a06c4f6c48f11c54e
Nicole Yu <nicoleyu@outlook.sg>
2020-12-14 16:48:39 +0800

    Clean code

--- app/src/main/java/com/infosys/stocktake/nfc/NfcReaderActivity.java
+++ app/src/main/java/com/infosys/stocktake/nfc/NfcReaderActivity.java
@@ -72,16 +72,6 @@ public class NfcReaderActivity extends AppCompatActivity {
         user = (User) getIntent().getSerializableExtra("UserIntent");
         loan = (Loan) getIntent().getSerializableExtra(AddLoanActivity.LOAN_INTENT_KEY);
 
-//        if (source.equals("profile")) {
-//            // Do profile things
-//            resolveProfileIntent();
-//        } else if (source.equals("loan")) {
-//            // Do add loan things
-//            // Accept loan object and add the nfcTag
-//            resolveLoanIntent();
-//        } else {
-//            // Shouldn't be opened
-//        }
     }
 
     @Override
@@ -140,7 +130,6 @@ public class NfcReaderActivity extends AppCompatActivity {
                             public void onSuccess(Void aVoid) {
                                 Toast.makeText(NfcReaderActivity.this, R.string.create_loan_success, Toast.LENGTH_SHORT).show();
                                 Intent detailsIntent = new Intent(NfcReaderActivity.this, LoanDetailsActivity.class);
-//                detailsIntent.putExtra("LoanIntent", currentLoan);
                                 Log.d(TAG,"PASSING LOAN ID: "+loan.getLoanID());
                                 itemRepo.query(loan.getItemID()).addOnSuccessListener(new OnSuccessListener<Item>() {
                                     @Override
@@ -246,108 +235,10 @@ public class NfcReaderActivity extends AppCompatActivity {
     private String dumpTagData(Tag tag) {
         StringBuilder sb = new StringBuilder();
         byte[] id = tag.getId();
-        sb.append("ID (hex): ").append(toHex(id)).append('\n');
-        sb.append("ID (reversed hex): ").append(toReversedHex(id)).append('\n');
-        sb.append("ID (dec): ").append(toDec(id)).append('\n');
-        sb.append("ID (reversed dec): ").append(toReversedDec(id)).append('\n');
-
-        String prefix = "android.nfc.tech.";
-        sb.append("Technologies: ");
-        for (String tech : tag.getTechList()) {
-            sb.append(tech.substring(prefix.length()));
-            sb.append(", ");
-        }
-
-        sb.delete(sb.length() - 2, sb.length());
-
-        for (String tech : tag.getTechList()) {
-            if (tech.equals(MifareClassic.class.getName())) {
-                sb.append('\n');
-                String type = "Unknown";
-
-                try {
-                    MifareClassic mifareTag = MifareClassic.get(tag);
-
-                    switch (mifareTag.getType()) {
-                        case MifareClassic.TYPE_CLASSIC:
-                            type = "Classic";
-                            break;
-                        case MifareClassic.TYPE_PLUS:
-                            type = "Plus";
-                            break;
-                        case MifareClassic.TYPE_PRO:
-                            type = "Pro";
-                            break;
-                    }
-                    sb.append("Mifare Classic type: ");
-                    sb.append(type);
-                    sb.append('\n');
-
-                    sb.append("Mifare size: ");
-                    sb.append(mifareTag.getSize() + " bytes");
-                    sb.append('\n');
-
-                    sb.append("Mifare sectors: ");
-                    sb.append(mifareTag.getSectorCount());
-                    sb.append('\n');
-
-                    sb.append("Mifare blocks: ");
-                    sb.append(mifareTag.getBlockCount());
-                } catch (Exception e) {
-                    sb.append("Mifare classic error: " + e.getMessage());
-                }
-            }
-
-            if (tech.equals(MifareUltralight.class.getName())) {
-                sb.append('\n');
-                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
-                String type = "Unknown";
-                switch (mifareUlTag.getType()) {
-                    case MifareUltralight.TYPE_ULTRALIGHT:
-                        type = "Ultralight";
-                        break;
-                    case MifareUltralight.TYPE_ULTRALIGHT_C:
-                        type = "Ultralight C";
-                        break;
-                }
-                sb.append("Mifare Ultralight type: ");
-                sb.append(type);
-            }
-        }
-
-//        return sb.toString();
         return String.valueOf(toDec(id));
     }
 
     // UTILS
-    private String toHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = bytes.length - 1; i >= 0; --i) {
-            int b = bytes[i] & 0xff;
-            if (b < 0x10)
-                sb.append('0');
-            sb.append(Integer.toHexString(b));
-            if (i > 0) {
-                sb.append(" ");
-            }
-        }
-        return sb.toString();
-    }
-
-    private String toReversedHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < bytes.length; ++i) {
-            if (i > 0) {
-                sb.append(" ");
-            }
-            int b = bytes[i] & 0xff;
-            if (b < 0x10)
-                sb.append('0');
-            sb.append(Integer.toHexString(b));
-        }
-        return sb.toString();
-    }
-
     private long toDec(byte[] bytes) {
         long result = 0;
         long factor = 1;
